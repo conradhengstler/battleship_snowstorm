b1a5f8c30abd5457c9bfb1cd8f375458dbd8c36f
Taylor <4970322@student.swin.edu.au>
2015-10-04 16:43:22 +1100

    made the new ai easy player and also fixed the setting options to have all three player types selectable

---
+++ src/Model/AIEasyPlayer.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+//using System.Data;
+using System.Diagnostics;
+using SwinGameSDK;
+
+public class AIEasyPlayer : AIPlayer
+{
+	public AIEasyPlayer (BattleShipsGame controller) : base(controller)
+	{
+	}
+
+	/// <summary>
+	/// GenerateCoordinates should generate random shooting coordinates
+	/// all the time.
+	/// </summary>
+	/// <param name="row">the generated row</param>
+	/// <param name="column">the generated column</param>
+	protected override void GenerateCoords(ref int row, ref int column)
+	{
+		do {
+			SearchCoords(ref row, ref column);
+		} while ((row < 0 || column < 0 || row >= EnemyGrid.Height || column >= EnemyGrid.Width || EnemyGrid[row, column] != TileView.Sea));
+		//while inside the grid and not a sea tile do the search
+	}
+
+	/// <summary>
+	/// SearchCoords will randomly generate shots within the grid as long as its not hit that tile already
+	/// </summary>
+	/// <param name="row">the generated row</param>
+	/// <param name="column">the generated column</param>
+	private void SearchCoords(ref int row, ref int column)
+	{
+		row = _Random.Next(0, EnemyGrid.Height);
+		column = _Random.Next(0, EnemyGrid.Width);
+	}
+
+	/// <summary>
+	/// ProcessShot will do nothing, as the Easy AI will not prepare for their next
+	/// shot based on the results.
+	/// </summary>
+	/// <param name="row">the row it needs to process</param>
+	/// <param name="col">the column it needs to process</param>
+	/// <param name="result">the result og the last shot (should be hit)</param>
+	protected override void ProcessShot(int row, int col, AttackResult result)
+	{
+	}
+}
